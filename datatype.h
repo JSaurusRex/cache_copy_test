f0d73cccf635f738ec9bf39bf56f7c9113cea682
JSaurusRex <jessetonckens@gmail.com>
2024-12-29 22:36:47 +0100

    initial commit

---
+++ datatype.h
@@ -0,0 +1,10 @@
+
+#define SIZE 5000
+#define ARRAY_SIZE 30000
+
+typedef struct
+{
+    long long int random_data[SIZE];
+    int data;
+    long long int random_data2[SIZE];
+} datatype;
---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "datatype.h"
+
+// #define SA 1
+
+
+void main()
+{
+    datatype * array1 = calloc(ARRAY_SIZE, sizeof(datatype));
+    datatype * array2 = calloc(ARRAY_SIZE, sizeof(datatype));
+
+    #ifdef SA
+        int * tmpArray = calloc(ARRAY_SIZE, sizeof(int));
+    #endif
+
+    //fill array1 with data
+    for(int i = 0; i < ARRAY_SIZE; i++)
+    {
+        array1[i].data = ARRAY_SIZE-i;
+    }
+
+    #ifdef SA
+        //copy over data to small array
+        for(int i = 0; i < ARRAY_SIZE; i++)
+        {
+            tmpArray[i] = array1[i].data;
+        }
+        
+        //copy over data from small array to big array
+        for(int i = 0; i < ARRAY_SIZE; i++)
+        {
+            array2[i].data = tmpArray[i];
+        }
+    #else
+        //copy over data
+        for(int i = 0; i < ARRAY_SIZE; i++)
+        {
+            array2[i].data = array1[i].data;
+        }
+    #endif
+
+    srand ( time(NULL) );
+    int index = rand() % ARRAY_SIZE;
+    printf("array entry[%i]: %i\n", index, array2[index].data);
+}
